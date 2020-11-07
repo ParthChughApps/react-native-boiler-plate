3c78c777b5edd1f528d77045fb45b927cad01a07
Parth Chugh <parthchugh52@gmail.com>
2020-11-08 01:53:45 +0530

    changed name to excelegal

--- android/app/src/main/java/com/accessai/examiner/MainActivity.java
+++ android/app/src/main/java/com/accessai/examiner/MainActivity.java
@@ -1,4 +1,4 @@
-package com.osheen;
+package com.accessai.examiner;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "osheen";
+    return "Excelegal";
   }
 }
